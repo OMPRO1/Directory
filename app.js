aaf003f78798fc4f9141142a7ff09e61ecba735e
Ruturaj Suresh Tarapurkar <ruturajtarapurkar100@gmail.com>
2024-05-28 21:00:01 +0530

    modified file

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/api",(req,res) => {
     res.send("Hello how are it's been long time i haven't seen you");
 });
 
+app.all("*",(req,res) => {
+    res.send("ERROR 404 Path not found!");
+});
+
 app.listen(PORT, () => {
     console.log("App is listening on port : ",PORT);
 });
+
